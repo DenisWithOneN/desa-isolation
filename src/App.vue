c5870af6b205811a27680daa58fe6ac1ee4319ab
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-05 00:20:33 +0300

    init project setup

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+  <p class="flex justify-center items-center min-h-screen">
+    welcome page - desa isolation
+  </p>
+</template>
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{vue,js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
