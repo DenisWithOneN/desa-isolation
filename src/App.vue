f3f361086df243a180e651b389490c7ad739d905
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-11 20:43:18 +0300

    Remove unused components from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
 import { RouterView } from "vue-router";
-import NavbarDesktop from "./components/general/NavbarDesktop.vue";
-import NavbarMobile from "./components/general/NavbarMobile.vue";
 </script>
 <template>
   <body class="bg-gray-700">
-    <NavbarDesktop />
-    <NavbarMobile />
     <RouterView></RouterView>
   </body>
 </template>
