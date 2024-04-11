d1875220f30a13954d17cea51831efefe036f881
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-11 21:03:34 +0300

    added container class and horizontal padding for the whole section

--- src/components/homepage/HeroSection.vue
+++ src/components/homepage/HeroSection.vue
@@ -3,9 +3,11 @@ import FillButton from "../general/FillButton.vue";
 import EmptyButton from "../general/EmptyButton.vue";
 </script>
 <template>
-  <section>
-    <div class="hero flex flex-col justify-center items-center gap-2">
-      <p class="text-white text-4xl font-black uppercase mb-2">Motto</p>
+  <section class="container mx-auto">
+    <div class="hero flex flex-col justify-center items-center gap-2 px-5">
+      <p class="text-white text-center text-3xl font-black uppercase mb-2">
+        put your motto here lorem ipsum
+      </p>
       <FillButton text="despre noi"></FillButton>
       <EmptyButton text="contact"></EmptyButton>
     </div>
