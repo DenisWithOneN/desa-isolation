4aab3fd378fdce66ecd69fa95944fc234e20e45b
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-11 21:10:01 +0300

    removed container class

--- src/components/homepage/HeroSection.vue
+++ src/components/homepage/HeroSection.vue
@@ -3,7 +3,7 @@ import FillButton from "../general/FillButton.vue";
 import EmptyButton from "../general/EmptyButton.vue";
 </script>
 <template>
-  <section class="container mx-auto">
+  <section>
     <div class="hero flex flex-col justify-center items-center gap-2 px-5">
       <p class="text-white text-center text-3xl font-black uppercase mb-2">
         put your motto here lorem ipsum
