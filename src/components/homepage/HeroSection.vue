a39d258dad65cd7948b6503901a5a4aa2074e79f
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-11 21:20:32 +0300

    updated design, responsive

--- src/components/homepage/HeroSection.vue
+++ src/components/homepage/HeroSection.vue
@@ -4,12 +4,21 @@ import EmptyButton from "../general/EmptyButton.vue";
 </script>
 <template>
   <section>
-    <div class="hero flex flex-col justify-center items-center gap-2 px-5">
-      <p class="text-white text-center text-3xl font-black uppercase mb-2">
-        put your motto here lorem ipsum
-      </p>
-      <FillButton text="despre noi" route="/about"></FillButton>
-      <EmptyButton text="contact" route="/contact"></EmptyButton>
+    <div
+      class="hero flex flex-col lg:flex-row justify-center lg:justify-end items-center gap-2 lg:gap-5 px-5"
+    >
+      <div class="flex flex-col items-center lg:items-end">
+        <p class="text-white text-center text-3xl font-black uppercase mb-2">
+          put your motto here lorem ipsum
+        </p>
+
+        <div class="flex flex-col gap-3 md:flex-row">
+          <FillButton text="despre noi" route="/about"></FillButton>
+          <EmptyButton text="contact" route="/contact"></EmptyButton>
+        </div>
+      </div>
+
+      <div class="bg-desalightgreen hidden lg:block w-2 h-32"></div>
     </div>
   </section>
 </template>
