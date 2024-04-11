b9ed9aba2dd83fe8d66ee970b60163bb968f5cfe
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-11 20:59:27 +0300

    Add EmptyButton, FillButton to the hero section

---
+++ src/components/general/EmptyButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  text: String,
+});
+</script>
+<template>
+  <div
+    class="border-desalightgreen border-4 flex justify-center items-center w-32 h-12 py-2 px-4"
+  >
+    <button class="text-white">{{ text }}</button>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/components/homepage/HeroSection.vue
@@ -0,0 +1,22 @@
+<script setup>
+import FillButton from "../general/FillButton.vue";
+import EmptyButton from "../general/EmptyButton.vue";
+</script>
+<template>
+  <section>
+    <div class="hero flex flex-col justify-center items-center gap-2">
+      <p class="text-white text-4xl font-black uppercase">Motto</p>
+      <FillButton text="despre noi"></FillButton>
+      <EmptyButton text="contact"></EmptyButton>
+    </div>
+  </section>
+</template>
+<style scoped>
+.hero {
+  background-image: url("public/images/DSC04976.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  aspect-ratio: 5/4;
+}
+</style>
