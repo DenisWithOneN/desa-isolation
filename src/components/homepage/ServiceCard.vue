c06a2f48b059c6c077478e233fde24c02ac51b52
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-13 16:34:23 +0300

    completed design for the card component

--- src/components/homepage/ServiceCard.vue
+++ src/components/homepage/ServiceCard.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  icon: String,
+  title: String,
+  text: String,
+});
+</script>
 <template>
-  <div class="bg-desagray h-52 w-52"></div>
+  <div class="bg-desagray h-52 w-52 flex flex-col items-center justify-center">
+    <div class="text-desalightgreen text-5xl mb-4">
+      <i :class="icon"></i>
+    </div>
+    <p class="text-xl text-center font-semibold">{{ title }}</p>
+    <p class="text-sm text-center px-2">
+      {{ text }}
+    </p>
+  </div>
 </template>
 <style scoped></style>
