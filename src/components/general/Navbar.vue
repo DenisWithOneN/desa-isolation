a2c93c28801645100203f1e608dc491723979f71
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-13 18:07:37 +0300

    added Navbar component, includes navbar desktop and navbar mobile

---
+++ src/components/general/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NavbarDesktop from "/src/components/general/NavbarDesktop.vue";
+import NavbarMobile from "/src/components/general/NavbarMobile.vue";
+</script>
+<template>
+  <NavbarDesktop />
+  <NavbarMobile />
+</template>
+<style scoped></style>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup>
-import NavbarDesktop from "/src/components/general/NavbarDesktop.vue";
-import NavbarMobile from "/src/components/general/NavbarMobile.vue";
+import Navbar from "../general/Navbar.vue";
 import HeroSection from "../homepage/HeroSection.vue";
 import ServiceSection from "../homepage/ServiceSection.vue";
 </script>
 <template>
-  <NavbarDesktop />
-  <NavbarMobile />
+  <Navbar />
   <HeroSection />
-  <ServiceSection />
+  <ServiceSection class="my-24" />
 </template>
 <style scoped></style>
