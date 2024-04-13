d5e52350537fb200e6e2d6f87399f220c092cd8b
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-13 16:13:51 +0300

    added all elements for section

---
+++ src/components/homepage/ServiceSection.vue
@@ -0,0 +1,23 @@
+<script setup>
+import FillButton from "../general/FillButton.vue";
+</script>
+<template>
+  <section>
+    <p>ce oferim</p>
+    <div>line</div>
+    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+    <p>
+      Mauris urna orci, ultricies vel laoreet ac, vehicula sit amet justo.
+      Integer eu lorem eget dolor bibendum lacinia a ac urna. Ut sed egestas
+      urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
+      faucibus tellus eget dolor bibendum lacinia a ac urna. Ut sed egestas
+      urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
+      faucibus tellus
+    </p>
+    <FillButton text="servicii" route="/services"></FillButton>
+    <div class="grid grid-cols-2">
+      <div v-for="i in 4" :key="index" class="bg-desagray h-32 w-32"></div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -2,10 +2,12 @@
 import NavbarDesktop from "/src/components/general/NavbarDesktop.vue";
 import NavbarMobile from "/src/components/general/NavbarMobile.vue";
 import HeroSection from "../homepage/HeroSection.vue";
+import ServiceSection from "../homepage/ServiceSection.vue";
 </script>
 <template>
   <NavbarDesktop />
   <NavbarMobile />
   <HeroSection />
+  <ServiceSection />
 </template>
 <style scoped></style>
