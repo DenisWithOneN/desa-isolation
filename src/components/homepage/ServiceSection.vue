2f2c22818226cf77d0e430edb81c82f9b6707203
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-13 16:59:34 +0300

    setup responsive text sizes

--- src/components/homepage/ServiceSection.vue
+++ src/components/homepage/ServiceSection.vue
@@ -3,22 +3,33 @@ import FillButton from "../general/FillButton.vue";
 import ServiceCard from "./ServiceCard.vue";
 </script>
 <template>
-  <section>
-    <p>ce oferim</p>
-    <div>line</div>
-    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-    <p>
-      Mauris urna orci, ultricies vel laoreet ac, vehicula sit amet justo.
-      Integer eu lorem eget dolor bibendum lacinia a ac urna. Ut sed egestas
-      urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
-      faucibus tellus eget dolor bibendum lacinia a ac urna. Ut sed egestas
-      urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
-      faucibus tellus
-    </p>
-    <FillButton text="servicii" route="/services"></FillButton>
+  <section class="container mx-auto">
+    <div class="flex flex-col items-center justify-center gap-3 mb-12 px-10">
+      <div class="flex flex-col items-center gap-2">
+        <p class="text-sm lg:text-xl">ce oferim?</p>
+        <div class="bg-desalightgreen w-12 h-1"></div>
+      </div>
 
-    <div class="flex justify-center">
-      <div class="grid grid-cols-1 gap-5 md:grid-cols-2 border-2">
+      <h2
+        class="text-2xl text-center font-black uppercase mb-2 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </h2>
+
+      <p
+        class="text-center text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl"
+      >
+        Mauris urna orci, ultricies vel laoreet ac, vehicula sit amet justo.
+        Integer eu lorem eget dolor bibendum lacinia a ac urna. Ut sed egestas
+        urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
+        faucibus tellus eget dolor bibendum lacinia a ac urna. Ut sed egestas
+        urna. Phasellus pharetra, sem in consectetur sollicitudin, purus velit
+        faucibus tellus
+      </p>
+    </div>
+
+    <div class="flex flex-col justify-center items-center gap-10">
+      <div class="grid grid-cols-1 gap-5 md:grid-cols-2">
         <ServiceCard
           :icon="`bi bi-dash-square`"
           :title="`Service One`"
@@ -44,6 +55,7 @@ import ServiceCard from "./ServiceCard.vue";
       non sequi quisquam`"
         ></ServiceCard>
       </div>
+      <FillButton text="servicii" route="/services"></FillButton>
     </div>
   </section>
 </template>
