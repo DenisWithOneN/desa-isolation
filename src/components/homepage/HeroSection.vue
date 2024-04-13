96cd38a2ad213b8f1b70cb0b8147112deb2bbcea
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-13 16:50:10 +0300

    changed h1 class order

--- src/components/homepage/HeroSection.vue
+++ src/components/homepage/HeroSection.vue
@@ -8,12 +8,12 @@ import EmptyButton from "../general/EmptyButton.vue";
       class="hero flex flex-col justify-center items-center gap-2 px-5 lg:flex-row lg:gap-5 lg:justify-end"
     >
       <div class="flex flex-col items-center lg:items-end">
-        <p
-          class="text-white text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl lg:text-end font-black uppercase mb-2"
+        <h1
+          class="text-white text-center text-2xl font-black uppercase mb-2 sm:text-4xl md:text-5xl lg:text-6xl lg:text-end xl:text-7xl"
         >
           put your motto here<br />
           lorem ipsum
-        </p>
+        </h1>
 
         <div class="flex flex-col gap-3 md:flex-row">
           <FillButton text="despre noi" route="/about"></FillButton>
