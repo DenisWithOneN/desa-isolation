50680056916cb4955059e0db0c1911f6b2e27191
DenisWithOneN <denis.ursut@emanuel.com>
2024-04-16 10:12:42 +0300

    updated card sizes

--- src/components/homepage/AdvantageCard.vue
+++ src/components/homepage/AdvantageCard.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="bg-white border-black border flex flex-col justify-between p-2 w-36 h-36 sm:w-52 sm:h-52 lg:w-60 lg:h-60 drop-shadow-lg">
+  <div class="bg-white border-black border flex flex-col justify-between p-2 w-36 h-36 sm:w-52 sm:h-52 lg:w-56 lg:h-56 xl:w-72 xl:h-72 drop-shadow-lg">
     <p class="text-end">01</p>
     <div>
     <span class="text-desalightgreen text-3xl"><i class="bi bi-square"</i></span>
