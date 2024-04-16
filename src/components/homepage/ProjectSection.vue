5096174b78324af13d5a4c5f6ad26aa4e17f9305
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-16 18:02:30 +0300

    added project section skeleton

--- src/components/homepage/ProjectSection.vue
+++ src/components/homepage/ProjectSection.vue
@@ -1,3 +1,15 @@
 <script setup></script>
-<template></template>
+<template>
+  <section class="bg-desalightgray py-24">
+    <div class="flex flex-col items-center gap-2">
+      <h2
+        class="text-desadarkgreen text-2xl text-center font-black uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+      >
+        proiecte
+      </h2>
+      <div class="bg-desalightgreen w-24 h-1" />
+      <div class="bg-desadarkgreen w-32 lg:w-96 h-52 mt-24">slider</div>
+    </div>
+  </section>
+</template>
 <style scoped></style>
