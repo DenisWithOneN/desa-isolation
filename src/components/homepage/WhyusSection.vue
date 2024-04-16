15aa8a027255a77125c8ec90d73616227531bdbb
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-16 18:09:59 +0300

    added section title and small title

--- src/components/homepage/WhyusSection.vue
+++ src/components/homepage/WhyusSection.vue
@@ -1,3 +1,18 @@
 <script setup></script>
-<template></template>
+<template>
+  <section class="bg-desadarkgreen py-24">
+    <div class="flex flex-col items-center gap-2">
+      <h2 class="text-white text-sm uppercase lg:text-md">
+        alegerea ta conteaza
+      </h2>
+      <div class="bg-desalightgreen w-32 h-1" />
+    </div>
+    <h2
+      class="text-white text-2xl text-center font-black uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+    >
+      de ce<br />desa isolation?
+    </h2>
+    <p class="text-white">garantam:</p>
+  </section>
+</template>
 <style scoped></style>
