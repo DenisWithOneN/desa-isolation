66ac452cb0fc04dbba156db6e8f4b20e51784d22
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-16 17:58:27 +0300

    update design and responsiveness

--- src/components/homepage/AdvantageCard.vue
+++ src/components/homepage/AdvantageCard.vue
@@ -1,12 +1,25 @@
 <script setup></script>
 <template>
-  <div class="bg-white border-black border flex flex-col justify-between p-2 w-36 h-36 sm:w-52 sm:h-52 lg:w-56 lg:h-56 xl:w-72 xl:h-72 drop-shadow-lg">
-    <p class="text-end">01</p>
+  <div
+    class="bg-white border-desadarkgray border flex flex-col justify-between p-2 sm:p-4 w-36 h-36 sm:w-52 sm:h-52 lg:w-56 lg:h-56 xl:w-72 xl:h-72 drop-shadow-lg"
+  >
+    <p class="text-desadarkgray text-sm xl:text-lg text-end">01</p>
     <div>
-    <span class="text-desalightgreen text-3xl"><i class="bi bi-square"</i></span>
-    <p>Service three</p>
-    <p>citeste mai mult</p>
-</div>
+      <span
+        class="text-desalightgreen text-3xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl"
+        ><i class="bi bi-square"></i
+      ></span>
+      <p
+        class="text-desalightgreen sm:text-2xl md:text-2xl lg:text-2xl xl:text-4xl font-semibold"
+      >
+        Service three
+      </p>
+      <p
+        class="text-desadarkgray text-sm sm:text-lg md:text-xl lg:text-xl xl:text-2xl"
+      >
+        citeste mai mult
+      </p>
+    </div>
   </div>
 </template>
 <style scoped></style>
