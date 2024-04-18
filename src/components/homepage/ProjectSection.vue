85e1930670a7c88ed0a28bfa87148caa811f2b39
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-18 18:24:00 +0300

    added project card

--- src/components/homepage/ProjectSection.vue
+++ src/components/homepage/ProjectSection.vue
@@ -1,14 +1,25 @@
-<script setup></script>
+<script setup>
+import ProjectCard from "../homepage/ProjectCard.vue";
+</script>
 <template>
   <section class="bg-desalightgray py-24">
-    <div class="flex flex-col items-center gap-2">
-      <h2
-        class="text-desadarkgreen text-2xl text-center font-black uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+    <div class="container mx-auto flex flex-col items-center gap-10 px-10">
+      <div class="flex flex-col items-center gap-2">
+        <h2
+          class="text-desadarkgreen text-2xl text-center font-black uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
+        >
+          proiecte
+        </h2>
+        <div class="bg-desalightgreen w-24 h-1" />
+      </div>
+
+      <div
+        class="flex flex-col gap-14 md:flex-row md:flex-wrap md:justify-around w-full"
       >
-        proiecte
-      </h2>
-      <div class="bg-desalightgreen w-24 h-1" />
-      <div class="bg-desadarkgreen w-32 lg:w-96 h-52 mt-24">slider</div>
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
     </div>
   </section>
 </template>
