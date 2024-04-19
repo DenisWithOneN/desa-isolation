4c6596dd54ef7b3c0eb86bf0cb523a491b942e85
DenisWithOneN <denis.ursut@emanuel.com>
2024-04-19 10:43:04 +0300

    update design responsiveness

--- src/components/homepage/WhyusSection.vue
+++ src/components/homepage/WhyusSection.vue
@@ -6,9 +6,9 @@ import WhyusReviewCard from "./WhyusReviewCard.vue";
   <section class="bg-desadarkgreen py-24">
     <div class="flex flex-col items-center gap-10">
       <div class="flex flex-col items-center gap-2">
-        <h2 class="text-white text-sm uppercase lg:text-md">
+        <p class="text-white text-sm uppercase lg:text-md">
           alegerea ta conteaza
-        </h2>
+        </p>
         <div class="bg-desalightgreen w-32 h-1" />
         <h2
           class="text-white text-2xl text-center font-black uppercase sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"
@@ -17,16 +17,19 @@ import WhyusReviewCard from "./WhyusReviewCard.vue";
         </h2>
       </div>
 
-      <p class="text-white">garantam:</p>
-      <div class="grid md:grid-cols-3 lg:grid-cols-2 gap-5">
-        <WhyusCard />
-        <WhyusCard />
-        <WhyusCard />
-        <WhyusCard />
-        <WhyusCard />
-        <WhyusCard />
+      <div class="flex flex-col items-center gap-10 lg:flex-row lg:gap-20">
+        <div
+          class="grid md:grid-cols-3 lg:grid-cols-2 gap-5 place-items-center"
+        >
+          <WhyusCard />
+          <WhyusCard />
+          <WhyusCard />
+          <WhyusCard />
+          <WhyusCard />
+          <WhyusCard />
+        </div>
+        <WhyusReviewCard />
       </div>
-      <WhyusReviewCard />
     </div>
   </section>
 </template>
