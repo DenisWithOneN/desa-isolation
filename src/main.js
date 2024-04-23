29f129c86500419c27c7cbb7ac8a3ecb0ccfdb89
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-23 17:01:40 +0300

    changed file path to style.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import "./style.css";
+import "@/style.css";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 
