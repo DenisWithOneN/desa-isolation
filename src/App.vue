79d21d30058cb1657c1caf1a3506c1657ab88b71
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-24 21:13:53 +0300

    adding dist

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { RouterView } from "vue-router";
-</script>
-<template>
-  <body>
-    <RouterView></RouterView>
-  </body>
-</template>
-<style scoped></style>
--- src/components/general/Navbar.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import NavbarDesktop from "/src/components/general/NavbarDesktop.vue";
-import NavbarMobile from "/src/components/general/NavbarMobile.vue";
-</script>
-<template>
-  <NavbarDesktop />
-  <NavbarMobile />
-</template>
-<style scoped></style>
