abce4cfd0914d0588ac94b2d6ac1766da612eee1
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-27 17:11:18 +0300

    changed view files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from "/src/components/general/Navbar.vue";
+import HeroSection from "/src/components/homepage/HeroSection.vue";
+import ServiceSection from "/src/components/homepage/ServiceSection.vue";
+import AboutSection from "/src/components/homepage/AboutSection.vue";
+import DefinitionSection from "/src/components/homepage/DefinitionSection.vue";
+import AdvantageSection from "/src/components/homepage/AdvantageSection.vue";
+import ProjectSection from "/src/components/homepage/ProjectSection.vue";
+import WhyusSection from "/src/components/homepage/WhyusSection.vue";
+import Footer from "/src/components/general/Footer.vue";
+</script>
+<template>
+  <Navbar />
+  <HeroSection />
+  <ServiceSection />
+  <AboutSection />
+  <DefinitionSection />
+  <AdvantageSection />
+  <ProjectSection />
+  <WhyusSection />
+  <Footer />
+</template>
+<style scoped></style>
