a945a23b393601568f51c082342b68717118c0f4
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-13 21:10:48 +0300

    changed style location for full coverage

--- src/components/general/EmptyButton.vue
+++ src/components/general/EmptyButton.vue
@@ -5,10 +5,10 @@ const props = defineProps({
 });
 </script>
 <template>
-  <button
+  <RouterLink
     class="border-desalightgreen border-4 text-white flex justify-center items-center w-32 h-12 py-2 px-4"
+    :to="route"
+    >{{ text }}</RouterLink
   >
-    <RouterLink :to="route">{{ text }}</RouterLink>
-  </button>
 </template>
 <style scoped></style>
--- src/components/general/FillButton.vue
+++ src/components/general/FillButton.vue
@@ -5,10 +5,10 @@ const props = defineProps({
 });
 </script>
 <template>
-  <button
+  <RouterLink
     class="bg-desalightgreen text-white font-semibold flex justify-center items-center w-32 h-12 py-2 px-4"
+    :to="route"
+    >{{ text }}</RouterLink
   >
-    <RouterLink :to="route">{{ text }}</RouterLink>
-  </button>
 </template>
 <style scoped></style>
