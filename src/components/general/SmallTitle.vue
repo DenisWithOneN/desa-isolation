95fefe210ea7ccedcb47b39d014ad87dde4f0fdd
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-17 17:33:23 +0300

    added small title component

---
+++ src/components/general/SmallTitle.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  text: String,
+});
+</script>
+<template>
+  <div class="flex flex-col items-center lg:items-start">
+    <p class="text-desadarkgray text-sm uppercase lg:text-md">{{ text }}</p>
+    <div class="bg-desalightgreen w-12 h-1 mt-1 lg:w-14"></div>
+  </div>
+</template>
+<style scoped></style>
