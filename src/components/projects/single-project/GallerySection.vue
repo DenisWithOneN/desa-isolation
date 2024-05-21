12ea14c2a3ec3ce4525259e953029aa59a09f5ca
DenisWithOneN <denis.ursut@emanuel.com>
2024-05-21 13:56:08 +0300

    created Gallery section responsive skeleton

---
+++ src/components/projects/single-project/GallerySection.vue
@@ -0,0 +1,23 @@
+<script setup>
+const images = [
+  { url: "https://picsum.photos/200/300" },
+  { url: "https://picsum.photos/200/400" },
+];
+</script>
+
+<template>
+  <section class="bg-desadarkgreen py-14">
+    <div class="container mx-auto flex justify-center px-8">
+      <div class="grid grid-cols-2 gap-5">
+        <img
+          v-for="(image, index) in images"
+          :key="index"
+          :src="image.url"
+          alt="Image"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
